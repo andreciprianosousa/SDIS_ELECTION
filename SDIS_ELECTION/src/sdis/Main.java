336f5ddd2510f0f1d4f50629c5778b632a18232d
andreciprianosousa <up201505376@fe.up.pt>
2019-11-24 07:40:14 +0000

    test

--- SDIS_ELECTION/src/sdis/Main.java
+++ SDIS_ELECTION/src/sdis/Main.java
@@ -3,7 +3,7 @@ package sdis;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Worl"); 
+		System.out.println("Hello, World"); 
 		peterBranch();
 	}
 	
