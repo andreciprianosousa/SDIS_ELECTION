1984b24f3b2ee954783419b02fcde05d6c477eb9
andreciprianosousa <up201505376@fe.up.pt>
2019-12-03 11:34:01 +0000

    "Merged" network stuff with algorithm

--- SDIS_ELECTION/src/logic/Node.java
+++ SDIS_ELECTION/src/logic/Node.java
@@ -1,10 +1,12 @@
 package logic;
 
+import java.io.Serializable;
+import java.net.InetAddress;
 import java.util.HashSet;
 
 import network.*;
 
-public class Node {
+public class Node implements Serializable{
 	
 	protected int nodeID;
 	protected ComputationIndex computationIndex;
@@ -18,10 +20,16 @@ public class Node {
 	protected float storedValue;
 	protected int storedId;
 	
+	// Used in mobile implementation
+	private float xMax;
+	private float yMax;
+	private float xCoordinate;
+	private float yCoordinate;
+	private float nodeRange;
 	protected int port;
 	protected String ipAddress;
 	
-	public Node (int nodeID, int port, String ipAddress) {
+	public Node (int nodeID, int port, String ipAddress, int[] dimensions) throws InterruptedException {
 		this.nodeID = nodeID;
 		this.port = port;
 		this.ipAddress = ipAddress;
@@ -31,11 +39,74 @@ public class Node {
 		// don't forget to increase num when starting election
 		this.computationIndex = new ComputationIndex(this.getNodeID(), 0, this.getNodeValue()); 
 		
+		this.xMax=dimensions[0];
+		this.yMax=dimensions[1];
+		this.nodeRange = dimensions[2];
+		this.neighbors = new HashSet <Node>();
+		
+		//Initial coordinates 
+		xCoordinate = (float) ((Math.random() * ((xMax - 0) + 1)) + 0);
+		yCoordinate = (float) ((Math.random() * ((yMax - 0) + 1)) + 0);
 		
 		new NodeListener(this).start();
 		new NodeTransmitter(this).start();
 		
 	}
+	
+	public void updateNeighbors(HelloMessage message, InetAddress ipaddress) {
+		int nodeMessageID = message.getNode().getNodeID();
+		//if message node is not this node
+
+		if(nodeMessageID != nodeID) {
+			//if this node does not contain the name in the message
+			if(!(neighbors.contains( message.getNode()))) {
+				//if message node is inside neighborhood
+				if(message.getNode().isInsideNeighborhood(this)) {
+					neighbors.add(message.getNode());
+					
+					System.out.print("Node " + this.getNodeID() + " is neighbor of: [");
+					for(Node neighbor : neighbors) {
+						System.out.print(neighbor.getNodeID()+ " ");
+					}
+					System.out.println("]");
+				}
+			}	
+		}
+	}
+	
+	public boolean isInsideNeighborhood(Node node) {
+		float distanceBetweenNodes;
+		
+		distanceBetweenNodes = (float) Math.sqrt(Math.pow((node.xCoordinate-xCoordinate),2) + Math.pow((node.yCoordinate-yCoordinate),2));
+		
+		if(distanceBetweenNodes <= node.nodeRange) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	@Override
+    public String toString() {
+        return Integer.toString(nodeID);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Node node = (Node) obj;
+        if(node.getNodeID()==this.getNodeID()) {
+        	return true;
+        }
+        else {
+        	return false;
+        }
+
+    }
+    
+    public int hashCode() {
+        return toString().hashCode();
+    }
 	
 	public int getNodeID() {
 		return nodeID;
@@ -111,6 +182,45 @@ public class Node {
 	}
 	public ComputationIndex getCP() {
 		return this.computationIndex;
+	}
+	public float getxMax() {
+		return xMax;
+	}
+
+	public void setxMax(float xMax) {
+		this.xMax = xMax;
+	}
+
+	public float getyMax() {
+		return yMax;
+	}
+
+	public void setyMax(float yMax) {
+		this.yMax = yMax;
+	}
+
+	public float getxCoordinate() {
+		return xCoordinate;
+	}
+
+	public void setxCoordinate(float xCoordinate) {
+		this.xCoordinate = xCoordinate;
+	}
+
+	public float getyCoordinate() {
+		return yCoordinate;
+	}
+
+	public void setyCoordinate(float yCoordinate) {
+		this.yCoordinate = yCoordinate;
+	}
+
+	public float getNodeRange() {
+		return nodeRange;
+	}
+
+	public void setNodeRange(float nodeRange) {
+		this.nodeRange = nodeRange;
 	}
 
 }
