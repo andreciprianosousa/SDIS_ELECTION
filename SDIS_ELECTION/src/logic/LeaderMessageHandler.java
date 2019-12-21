85411533827195e7d5bbeb0c5df6f6c599f70fbf
andreciprianosousa <up201505376@fe.up.pt>
2019-12-21 10:23:09 +0000

    fixed election status becoming true and messing remotion

--- SDIS_ELECTION/src/logic/LeaderMessageHandler.java
+++ SDIS_ELECTION/src/logic/LeaderMessageHandler.java
@@ -64,75 +64,78 @@ public class LeaderMessageHandler extends Thread {
 			}
 		}
 
-		// This if prevents infinite leader messages
-		if (leaderMessage.getStoredID() == node.getLeaderID()) {
+//		// This if prevents infinite leader messages
+//		if (leaderMessage.getStoredID() == node.getLeaderID()) {
+//
+//			if (DEBUG)
+//				System.out.println("My Leader is " + node.getLeaderID() + " and the leader message says "
+//						+ leaderMessage.getStoredID() + ". Do nothing.");
+//
+//			node.setElectionActive(false);
+//			node.setParentActive(-1);
+//			node.setAckStatus(true);
+//			// Ready for new election
+//			node.setStoredId(node.getNodeID());
+//			node.setStoredValue(node.getNodeValue());
+//
+//			return;
+//		} else { // Either leaderMessage ID > node Leader or other way around...
+
+		// If this node receives leader message, update parameters accordingly if
+		// necessary
+		// And if has a different leader, broadcasts the leader msgs
+		if (node.isElectionActive() && (!(leaderMessage.isSpecial()))) {
 
 			if (DEBUG)
-				System.out.println("My Leader is " + node.getLeaderID() + " and the leader message says "
-						+ leaderMessage.getStoredID() + ". Do nothing.");
+				System.out.println("Receiving leader assurance from " + leaderMessage.getIncomingId());
 
 			node.setElectionActive(false);
+			node.setLeaderID(leaderMessage.getStoredID());
+			node.setLeaderValue(leaderMessage.getStoredValue());
 			node.setParentActive(-1);
 			node.setAckStatus(true);
 			// Ready for new election
 			node.setStoredId(node.getNodeID());
 			node.setStoredValue(node.getNodeValue());
 
-			return;
-		} else { // Either leaderMessage ID > node Leader or other way around...
-
-			// If this node receives leader message, update parameters accordingly if
-			// necessary
-			// And if has a different leader, broadcasts the leader msgs
-			if (node.isElectionActive() && (!(leaderMessage.isSpecial()))) {
-
-				if (DEBUG)
-					System.out.println("Receiving leader assurance from " + leaderMessage.getIncomingId());
-
-				node.setElectionActive(false);
-				node.setLeaderID(leaderMessage.getStoredID());
-				node.setLeaderValue(leaderMessage.getStoredValue());
-				node.setParentActive(-1);
-				node.setAckStatus(true);
-				// Ready for new election
-				node.setStoredId(node.getNodeID());
-				node.setStoredValue(node.getNodeValue());
-
-				if (DEBUG) {
-					System.out.println("Node " + node.getNodeID() + "'s leader is " + node.getLeaderID());
-					System.out.println("CP(num/value/id): " + node.getComputationIndex().getNum() + " - "
-							+ node.getComputationIndex().getValue() + " - " + node.getComputationIndex().getId());
-					System.out.println("-----------------------------");
-				}
-				// If my only neighbour is my parent, don't propagate leader message and just
-				// return
-				if (node.getNeighbors().size() == 1) {
-					return;
-				}
-
-				sendLeaderMessage();
-			} else {
-
-				if (leaderMessage.getStoredID() <= node.getLeaderID()) {
-					System.out.println("Men, that guy is weak. Do nothing!");
-
-					return;
-				}
-
-				node.setLeaderID(leaderMessage.getStoredID());
-				node.setLeaderValue(leaderMessage.getStoredValue());
-				node.setStoredId(node.getNodeID());
-				node.setStoredValue(node.getNodeValue());
-
-				// If my only neighbour is my parent, don't propagate leader message and just
-				// return
-				if (node.getNeighbors().size() == 1) {
-					return;
-				}
-
-				// If not, send leader messages to neighbours except to the message sender's id
-				sendLeaderMessage();
+			if (DEBUG) {
+				System.out.println("Node " + node.getNodeID() + "'s leader is " + node.getLeaderID());
+				System.out.println("CP(num/value/id): " + node.getComputationIndex().getNum() + " - "
+						+ node.getComputationIndex().getValue() + " - " + node.getComputationIndex().getId());
+				System.out.println("-----------------------------");
 			}
+			// If my only neighbour is my parent, don't propagate leader message and just
+			// return
+			if (node.getNeighbors().size() == 1) {
+				return;
+			}
+
+			sendLeaderMessage();
+		} else if (!(node.isElectionActive()) && !(leaderMessage.isSpecial())) {
+			if (DEBUG)
+				System.out.println("Not election and not special, end.");
+			return;
+		} else {
+
+			if (leaderMessage.getStoredID() <= node.getLeaderID()) {
+				System.out.println("Men, that guy is weak. Do nothing!");
+
+				return;
+			}
+
+			node.setLeaderID(leaderMessage.getStoredID());
+			node.setLeaderValue(leaderMessage.getStoredValue());
+			node.setStoredId(node.getNodeID());
+			node.setStoredValue(node.getNodeValue());
+
+			// If my only neighbour is my parent, don't propagate leader message and just
+			// return
+			if (node.getNeighbors().size() == 1) {
+				return;
+			}
+
+			// If not, send leader messages to neighbours except to the message sender's id
+			sendLeaderMessage();
 		}
 	}
 }
