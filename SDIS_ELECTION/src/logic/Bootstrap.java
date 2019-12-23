fc7dcd4146fb2cfc23956589afa4b44f32cee300
andreciprianosousa <up201505376@fe.up.pt>
2019-12-23 17:34:31 +0000

    removed outdated bootstrap code, may be useful still

--- SDIS_ELECTION/src/logic/Bootstrap.java
+++ SDIS_ELECTION/src/logic/Bootstrap.java
@@ -33,56 +33,59 @@ public class Bootstrap extends Thread {
 			System.out.println("I'm alone, my current Leader is me: " + node.getLeaderID());
 			System.out.println("-----------------------------");
 		}
+
+		// OUTDATED CODE, BUT MAY STILL BE USEFUL IN THE INITIAL BOOTSTRAP IF WE NEED TO
 		// If not, start an election with neighbours or join current network
-		else {
-
-			// Only biggest ID node should bootstrap election, should many nodes instantiate
-			// at once, given
-			// time to setup network beforehand
-			if (!(node.getNodeID() > node.getMaximumIdNeighbors())) {
-				try {
-					Thread.sleep(NetworkSet_Delay + Election_Delay); // Gives time to Higher Value node to finish the
-																		// election, subject to change with network size
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				if (node.getLeaderID() == node.getNodeID()) { // Either I'm a new node (No interaction with Elections)
-																// or i'm leader
-
-					if (DEBUG)
-						System.out.println(
-								"Node is not strong enough to initiate election. Exchanging leader info with biggest neighbour.");
-
-					new Handler(this.node, logic.MessageType.INFO, node.getMaximumIdNeighbors()).start();
-				}
-
-			} else { // If we're the biggest node of the network
-
-				node.setElectionActive(true);
-
-				// For every neighbour put them in waitingAck
-				synchronized (this) {
-					Iterator<Integer> i = node.getNeighbors().iterator();
-					while (i.hasNext()) {
-						Integer temp = i.next();
-						if ((!(node.getWaitingAcks().contains(temp))) && (!(temp.toString().equals("")))) {
-							node.getWaitingAcks().add(temp);
-						}
-					}
-				}
-				if (DEBUG)
-					System.out.println("Node " + node.getNodeID() + " bootstrapped election group message.");
-
-				node.simNode.setStart();
-
-				// -----------CP Tests-----------
-				node.getComputationIndex().setNum(node.getComputationIndex().getNum() + 1);
-				node.getComputationIndex().setId(node.getNodeID());
-				node.getComputationIndex().setValue(node.getNodeValue());
-				// ------------------------------
-				new Handler(this.node, logic.MessageType.ELECTION_GROUP, node.getWaitingAcks()).start();
-			}
-		}
+//		else {
+//
+//			// Only biggest ID node should bootstrap election, should many nodes instantiate
+//			// at once, given
+//			// time to setup network beforehand
+//			if (!(node.getNodeID() > node.getMaximumIdNeighbors())) {
+//				try {
+//					Thread.sleep(NetworkSet_Delay + Election_Delay); // Gives time to Higher Value node to finish the
+//																		// election, subject to change with network size
+//				} catch (InterruptedException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
+//				if (node.getLeaderID() == node.getNodeID()) { // Either I'm a new node (No interaction with Elections)
+//																// or i'm leader
+//
+//					if (DEBUG)
+//						System.out.println(
+//								"Node is not strong enough to initiate election. Exchanging leader info with biggest neighbour.");
+//
+//					new Handler(this.node, logic.MessageType.INFO, node.getMaximumIdNeighbors()).start();
+//				}
+//
+//			} else { // If we're the biggest node of the network; handy for initial bootstrap but
+//						// useless afterwards
+//
+//				node.setElectionActive(true);
+//
+//				// For every neighbour put them in waitingAck
+//				synchronized (this) {
+//					Iterator<Integer> i = node.getNeighbors().iterator();
+//					while (i.hasNext()) {
+//						Integer temp = i.next();
+//						if ((!(node.getWaitingAcks().contains(temp))) && (!(temp.toString().equals("")))) {
+//							node.getWaitingAcks().add(temp);
+//						}
+//					}
+//				}
+//				if (DEBUG)
+//					System.out.println("Node " + node.getNodeID() + " bootstrapped election group message.");
+//
+//				node.simNode.setStart();
+//
+//				// -----------CP Tests-----------
+//				node.getComputationIndex().setNum(node.getComputationIndex().getNum() + 1);
+//				node.getComputationIndex().setId(node.getNodeID());
+//				node.getComputationIndex().setValue(node.getNodeValue());
+//				// ------------------------------
+//				new Handler(this.node, logic.MessageType.ELECTION_GROUP, node.getWaitingAcks()).start();
+//			}
+//		}
 	}
 }
